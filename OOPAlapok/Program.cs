2999d22120dc04a6c0e080cc9d1c4756524e941f
poraczkizs <poraczkizs@kkszki.hu>
2025-09-08 11:54:10 +0200

    Szemely osztaly

--- OOPAlapok/Program.cs
+++ OOPAlapok/Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace OOPAlapok
 {
+    public class Szemely
+    {
+        public string nev = "Peti";
+        public int kor = 22;
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Szemely person = new Szemely();
+            Console.WriteLine($"A személy neve: {person.nev} és életkora: {person.kor}");
         }
+
+
     }
 }
